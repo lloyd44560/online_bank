cdeb019ff26ed39bf4eab6b899523b83e93b83bd
Your Name <jhonlloydborlagdan8@gmail.com>
2024-07-15 23:01:01 +0800

    Added the whole project file

---
+++ OnlineBank/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineBank.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
